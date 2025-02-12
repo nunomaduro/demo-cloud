4cb248ea2330e0cc925c3183f4d440c6d4146dfa
Nuno Maduro <enunomaduro@gmail.com>
2025-02-12 15:32:58 +0000

    First commit

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,24 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Benchmark;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
+
+// create a status endpoint that query the databases:
+Route::get('/status', function () {
+    $time = Benchmark::measure(function () {
+        User::factory()->create();
+    });
+
+    return response()->json([
+        'status' => 200,
+        'timeDoingAnInsert' => $time,
+        'usersCount' => User::query()->count(),
+    ]);
+});
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
